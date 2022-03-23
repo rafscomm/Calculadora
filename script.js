dc3d85a87142dae52a5042609f5aff2f32984b6f
rafscomm <rafaelpereira2033@gmail.com>
2022-03-23 11:08:44 -0300

    quase pronta

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 function insert(num){
  var numeros = document.getElementById("resposta").innerHTML;
- document.getElementById("resposta").innerHTML = num + numeros;
+ document.getElementById("resposta").innerHTML = numeros + num;
 
+}
+
+function apagar(){
+  document.getElementById("resposta").innerHTML="";
+}
+function back(){
+  var string = document.getElementById('resposta').innerHTML;
+  document.getElementById('resposta').innerHTML = string.substring(0,string.length -1);
 }
