ac5688dbe5741cce77407f269997397f3d1bf9dd
unknown <rafaelpereira2033@gmail.com>
2022-03-25 15:53:43 -0300

    resultado quase final

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ function back(){
   var string = document.getElementById('resposta').innerHTML;
   document.getElementById('resposta').innerHTML = string.substring(0,string.length -1);
 }
+function calcular(){
+  var calculo = document.getElementById('resposta').innerHTML;
+  if(calculo){
+    document.getElementById('resposta').innerHTML = eval(calculo);
+  }
+}
